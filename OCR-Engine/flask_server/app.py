f3026e37710b3c6538bea357248d28651fb914f5
kenAlparslan <47366236+aslanken@users.noreply.github.com>
2020-08-11 22:49:31 -0400

    Add POST endpoint for the file upload, define body parameters

--- OCR-Engine/flask_server/app.py
+++ OCR-Engine/flask_server/app.py
@@ -14,11 +14,29 @@ _VERSION = 1  # API version
 
 @app.route('/')
 def main():
+    '''
+     Endpoint for the app.
+         Parameters:
+                 None
+ 
+         Returns:
+                 [HTML] index.html
+     '''
     return render_template('index.html')
 
 
 @app.route('/v{}/ocr'.format(_VERSION), methods=["POST"])
 def ocr():
+    '''
+    Endpoint for OCR.
+
+        Parameters:
+                None
+
+        Returns:
+                (JSON): jsonify({"output": output}) if successful
+                (JSON): jsonify({"error": error}) if any error
+    '''
 
     # Read the URL
     try:
@@ -57,11 +75,30 @@ def ocr():
 
 @app.errorhandler(500)
 def internal_error(error):
+    '''
+    500 Endpoint for OCR. 
+
+        Parameters:
+                None
+
+        Returns:
+                None
+    '''
+    
     print("*** 500 ***\n{}".format(str(error)))  # ghetto logging
 
 
 @app.errorhandler(404)
 def not_found_error(error):
+    '''
+    404 Endpoint for OCR. 
+
+        Parameters:
+                None
+
+        Returns:
+                None
+    '''
     print("*** 404 ***\n{}".format(str(error)))
 
 if not app.debug:
